cd9cbf4a0979722614ac7e51dedddaf7a04f198b
mitsu-ksgr <k.sgr.mitsu.g@gmail.com>
2020-06-14 08:07:08 +0900

    add migrations: users table

---
+++ db/versions/0396244e8456_create_users_table.py
@@ -0,0 +1,32 @@
+"""create users table
+
+Revision ID: 0396244e8456
+Revises:
+Create Date: 2020-06-13 22:17:20.661439+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0396244e8456'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+    op.create_table(
+        'users',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('name', sa.String(50), nullable=False),
+        sa.Column('profile', sa.Unicode(200)),
+        sa.Column('created_at', sa.DateTime, nullable=False),
+        sa.Column('updated_at', sa.DateTime, nullable=False),
+    )
+
+def downgrade():
+    op.drop_table('users')
+
