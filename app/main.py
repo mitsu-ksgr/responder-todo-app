0f35fa81f704528b59b8c69272636f57fbaa3af0
mitsu-ksgr <k.sgr.mitsu.g@gmail.com>
2020-06-14 13:44:56 +0900

    Add user model

--- app/main.py
+++ app/main.py
@@ -22,6 +22,51 @@ def hello_to(req, resp, *, who):
 def hello_html(req, resp, *, who):
     resp.html = api.template('hello.html', who=who)
 
+#
+# DB connection test
+#
+@api.route("/users/new")
+def user_add(req, resp):
+    import sqlalchemy
+    import sqlalchemy.orm
+    url = 'mysql+mysqlconnector://dev_user:password@db:3306/app'
+    engine = sqlalchemy.create_engine(url, echo=False)
+
+    # make session
+    Session = sqlalchemy.orm.sessionmaker(bind=engine)
+    session = Session()
+
+    from app.models import users
+
+    name = req.params.get('name', 'test-user')
+    profile = req.params.get('profile', 'profile text')
+
+    user = users.User(name=name, profile=profile)
+    session.add(user)
+    session.commit()
+    resp.text = "User Add: {}".format(user)
+
+@api.route("/users")
+def user_list(req, resp):
+    import sqlalchemy
+    import sqlalchemy.orm
+    url = 'mysql+mysqlconnector://dev_user:password@db:3306/app'
+    engine = sqlalchemy.create_engine(url, echo=False)
+
+    # make session
+    Session = sqlalchemy.orm.sessionmaker(bind=engine)
+    session = Session()
+
+    from app.models import users
+
+    text = ""
+    for user in session.query(users.User).all():
+        text += f"{user}\n"
+
+    resp.text = text
+    session.close()
+
+
 if __name__ == '__main__':
     api.run()
 
---
+++ app/models/users.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+from datetime import datetime
+
+from sqlalchemy import Column, String, DateTime, text
+from sqlalchemy.dialects.mysql import INTEGER
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql.functions import current_timestamp
+
+class User(declarative_base()):
+    __tablename__ = 'users'
+
+    id = Column(
+        INTEGER(unsigned=True),
+        primary_key=True,
+        autoincrement=True
+    )
+    name = Column(String(50))
+    profile = Column(String(200, convert_unicode=True))
+    created_at = Column(
+        DateTime,
+        default=datetime.now(),
+        nullable=False,
+        server_default=current_timestamp()
+    )
+    updated_at = Column(
+        DateTime,
+        default=datetime.now(),
+        nullable=False,
+        onupdate=datetime.now()
+    )
+
+    def __repr__(self):
+        return "<User(id={}, name='{}', created_at='{}', updated_at='{}')>"\
+                .format(self.id, self.name, self.created_at, self.updated_at)
+
+
