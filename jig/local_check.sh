c21c59a5c092282c214d487d1d9fcf6debea17d7
mitsu-ksgr <k.sgr.mitsu.g@gmail.com>
2020-06-18 17:22:16 +0900

    improve jig

--- jig/local_check.sh
+++ jig/local_check.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -eu
 
+if [ "${1+$1}" = "-i" ]; then
+    # install dependencies
+    docker-compose exec web pipenv install --system --dev
+fi
+
 echo "----- Lint with flake8 -----"
-docker-compose exec web pipenv run \
-    flake8 --count --exit-zero app/*.py app/*/*.py
+docker-compose exec web pipenv run flake8 --exit-zero app/*.py app/*/*.py
 
 echo "----- Format with black -----"
 docker-compose exec web pipenv run black app/*.py app/*/*.py
