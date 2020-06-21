7e01ed5905f71545e2770e4a5167b2eea75d7577
mitsu-ksgr <k.sgr.mitsu.g@gmail.com>
2020-06-21 16:51:21 +0900

    improve config packages

--- config/__init__.py
+++ config/__init__.py
@@ -2,18 +2,7 @@
 # App config
 #
 
-import configparser
+from config.app_config import AppConfig
 
-def __load_config(file_path):
-    config = configparser.ConfigParser()
-    config.read(file_path)
-    return config
-
-app_config = __load_config('./config/app.ini')
-app_config.set('db', 'url', '{}+{}://{}:{}@{}:{}/{}'.format(
-    app_config.get('db', 'dialect'), app_config.get('db', 'driver'),
-    app_config.get('db', 'username'), app_config.get('db', 'password'),
-    app_config.get('db', 'host'), app_config.get('db', 'port'),
-    app_config.get('db', 'database')
-))
+app_config = AppConfig()
 
